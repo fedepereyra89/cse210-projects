61a0a0ca8db77c62bfa1eb605b5fd8169faf7c15
fedepereyra89 <fedepereyra89@gmail.com>
2023-12-02 22:34:01 -0300

    I wrote the code

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,42 @@
+namespace Develop05
+{
+    public abstract class Goals
+    {
+        private string _name;
+        private string _description;
+        private int _points;
+        
+        public void SetName(string name)
+        {
+            this._name = name;
+        }
+
+        public string GetName()
+        {
+            return this._name;
+        }
+
+        public void SetDescription(string description)
+        {
+            this._description = description;
+        }
+
+        public string GetDescription()
+        {
+            return this._description;
+        }
+
+        public void SetPoints(int points)
+        {
+            this._points = points;
+        }
+
+        public int GetPoints()
+        {
+            return this._points;
+        }
+
+        public abstract List<string> CreateGoal();
+
+    }
+}
