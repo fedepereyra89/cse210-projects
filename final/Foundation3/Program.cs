e97d6701f7a03f5dc4239a6e64be3df550a3d214
fedepereyra89 <fedepereyra89@gmail.com>
2023-12-12 22:34:17 -0300

    I wrote the code according to the instructions

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
+namespace Foundation3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        static void Main()
+        {
+            Address address = new Address("Sanchez de Loria 2105", "Parque Patricios", "Buenos Aires", "Argentina");
+            Lecture lecture = new Lecture("Devotional", "Come hear the Prophet Speak!", "10-10-2023", "11:15 AM", address, "President Russel M. Nelson", 300);
+            Reception reception = new Reception("Wedding", "Congratulations!", "September", "20:00 PM", address, "Weddingday@centralpark.com");
+            Outdoor outdoor = new Outdoor("Ward Picnic", "Come say hello to the ward!", "July 25", "12:00 PM", address, "Sunny weather. Bring a hat and sunscreen.");
+
+            lecture.GetStandardDetails();
+            lecture.GetFullDetails();
+            lecture.GetShortDescription();
+
+            reception.GetStandardDetails();
+            reception.GetFullDetails();
+            reception.GetShortDescription();
+
+            outdoor.GetStandardDetails();
+            outdoor.GetFullDetails();
+            outdoor.GetShortDescription();
+
+        }
     }
 }
